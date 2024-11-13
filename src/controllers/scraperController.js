9dad434387162763ab66973580314effb6de4f84
Aayush Rajendra Sawant <sawant.aay@northeastern.edu>
2024-11-13 15:21:03 -0500

    second commit

--- src/controllers/scraperController.js
+++ src/controllers/scraperController.js
@@ -1,30 +1,4 @@
-// const scraperService = require("../services/scraperService");
 
-// exports.scrapeWebsite = async (req, res, next) => {
-//     try {
-//       const { url } = req.body;
-  
-//       if (!url || !/^https?:\/\/[^\s$.?#].[^\s]*$/.test(url)) {
-//         return res.status(400).json({ error: "Invalid URL format" });
-//       }
-  
-//       const { csvFilePath } = await scraperService.scrape(url);
-  
-//       console.log("CSV File Path from Service:", csvFilePath); // Debugging
-  
-//       if (!csvFilePath || typeof csvFilePath !== "string") {
-//         return res.status(500).json({
-//           error: "Internal Server Error",
-//           details: "CSV file path is undefined or invalid",
-//         });
-//       }
-  
-//       res.json({ csvFilePath: `/temp/${path.basename(csvFilePath)}` });
-//     } catch (error) {
-//       console.error("Error in scrapeWebsite:", error.message); // Debugging
-//       next(error);
-//     }
-//   };
 const scraperService = require("../services/scraperService");
 const path = require("path");
 const fs = require("fs-extra");
